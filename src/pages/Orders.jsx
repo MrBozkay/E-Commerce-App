ac37d35f4c8309b7ec1f4d26dc19e7a3835e502d
mustafa bozkaya <mustafaegebozkaya555.gmail.com>
2024-09-19 13:59:47 +0300

    Add src/pages/Orders.jsx

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Orders = () => {
+  return (
+    <div>
+
+        <h1>Orders</h1>
+        <p>login page</p>
+        <form>
+            <input type="text" placeholder='username' />
+            <input type="password" placeholder='password' />
+            <button type='submit'>Orders</button>
+        </form>
+        
+    </div>
+  )
+}
+
+export default Orders
