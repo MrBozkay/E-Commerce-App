ab572f25e52170df57eb9b8a504828e34770aba8
mustafa bozkaya <mustafaegebozkaya555.gmail.com>
2024-09-19 13:59:46 +0300

    Add src/pages/Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+
+        <h1>Login</h1>
+        <p>login page</p>
+        <form>
+            <input type="text" placeholder='username' />
+            <input type="password" placeholder='password' />
+            <button type='submit'>Login</button>
+        </form>
+        
+    </div>
+  )
+}
+
+export default Login
