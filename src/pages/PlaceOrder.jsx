17f54dcfd613e73ff1b07e81286659b57331e509
mustafa bozkaya <mustafaegebozkaya555.gmail.com>
2024-09-19 13:59:47 +0300

    Add src/pages/PlaceOrder.jsx

---
+++ src/pages/PlaceOrder.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const PlaceOrder = () => {
+  return (
+    <div>
+
+        <h1>PlaceOrder</h1>
+        <p>login page</p>
+        <form>
+            <input type="text" placeholder='username' />
+            <input type="password" placeholder='password' />
+            <button type='submit'>PlaceOrder</button>
+        </form>
+        
+    </div>
+  )
+}
+
+export default PlaceOrder
