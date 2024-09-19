9a36a76a766191c97412f17478bf8899d580c24a
mustafa bozkaya <mustafaegebozkaya555.gmail.com>
2024-09-19 13:59:46 +0300

    Add src/pages/Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Hero1 from '../components/Hero1';
+
+
+import LatestCollection from '../components/LatestCollection';
+import BestSeller from '../components/BestSeller';
+import OurPolicy from '../components/OurPolicy';
+import NewsLetterBox from '../components/NewsLetterBox';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      
+      <LatestCollection/>
+      <BestSeller/>
+      <OurPolicy/>
+      <NewsLetterBox/>
+    </div>
+  )
+}
+
+export default Home
