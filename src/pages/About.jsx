befab29ae60b745b1716b89873c23a534cf6b337
mustafa bozkaya <mustafaegebozkaya555.gmail.com>
2024-09-19 13:59:46 +0300

    Add src/pages/About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+
+        <h1>About</h1>
+        <p>login page</p>
+        <form>
+            <input type="text" placeholder='username' />
+            <input type="password" placeholder='password' />
+            <button type='submit'>About</button>
+        </form>
+        
+    </div>
+  )
+}
+
+export default About
