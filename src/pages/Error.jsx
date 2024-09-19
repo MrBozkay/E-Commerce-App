62cf798f5e2fc92d2828db208375f7281eb17655
mustafa bozkaya <mustafaegebozkaya555.gmail.com>
2024-09-19 13:59:46 +0300

    Add src/pages/Error.jsx

---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+  <div className='error'>
+    <h1>404</h1>
+    <h2>Page Not Found</h2>
+    <p>Sorry, the page you are looking for does not exist.</p>
+  </div>
+  )
+}
+  export default Error;
