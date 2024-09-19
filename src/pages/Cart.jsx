66a505839933b74d9691ec9ec9eaa117a58d6836
mustafa bozkaya <mustafaegebozkaya555.gmail.com>
2024-09-19 13:59:46 +0300

    Add src/pages/Cart.jsx

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of our React App.</p>
+      <p>Click on the 'About' link to see the about page.</p>
+    </div>
+  )
+}
+
+export default Cart
